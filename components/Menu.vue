1f51a8abff7dbca0b1238afc16017615d9d36584
Yecely <yecely.diaz@rappi.com>
2020-06-01 12:49:13 -0500

    Menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -35,6 +35,7 @@
         </li>
       </ul>
     </div>
+    
   </menu>
 </template>
 
--- pages/sections/_slug.vue
+++ pages/sections/_slug.vue
@@ -6,7 +6,6 @@
           <div class="sidebar-fixed-width uk-visible@m">
             <Menu />
           </div>
-
           <div class="uk-width-1-1 uk-width-expand@m">
             <article class="uk-article">
               <h1 class="uk-article-title">{{ post.attributes.title }}</h1>
@@ -46,99 +45,6 @@
             </article>
           </div>
         </div>
-      </div>
-    </div>
-
-    <div id="offcanvas-docs" data-uk-offcanvas="overlay: true">
-      <div class="uk-offcanvas-bar">
-        <button class="uk-offcanvas-close" type="button" data-uk-close></button>
-        <h5 class="uk-margin-top">Científico de Datos</h5>
-        <ul class="uk-nav uk-nav-default doc-nav">
-          <li class="uk-active">
-            <a href="doc.html">Template setup</a>
-          </li>
-          <li>
-            <a href="doc.html">Basic theme setup</a>
-          </li>
-          <li>
-            <a href="doc.html">Navigation bar</a>
-          </li>
-          <li>
-            <a href="doc.html">Footer options</a>
-          </li>
-          <li>
-            <a href="doc.html">Creating your first post</a>
-          </li>
-          <li>
-            <a href="doc.html">Creating docs posts</a>
-          </li>
-          <li>
-            <a href="doc.html">Enabling comments</a>
-          </li>
-          <li>
-            <a href="doc.html">Google Analytics</a>
-          </li>
-        </ul>
-        <h5 class="uk-margin-top">Product Features</h5>
-        <ul class="uk-nav uk-nav-default doc-nav">
-          <li>
-            <a href="doc.html">Hero page header</a>
-          </li>
-          <li>
-            <a href="doc.html">Category boxes section</a>
-          </li>
-          <li>
-            <a href="doc.html">Fearured docs section</a>
-          </li>
-          <li>
-            <a href="doc.html">Video lightbox boxes section</a>
-          </li>
-          <li>
-            <a href="doc.html">Frequently asked questions section</a>
-          </li>
-          <li>
-            <a href="doc.html">Team members section</a>
-          </li>
-          <li>
-            <a href="doc.html">Call to action section</a>
-          </li>
-          <li>
-            <a href="doc.html">Creating a changelog</a>
-          </li>
-          <li>
-            <a href="doc.html">Contact form</a>
-          </li>
-          <li>
-            <a href="doc.html">Adding media to post and doc content</a>
-          </li>
-          <li>
-            <a href="doc.html">Adding table of contents to docs</a>
-          </li>
-          <li>
-            <a href="doc.html">Adding alerts to content</a>
-          </li>
-        </ul>
-        <h5 class="uk-margin-top">Customization</h5>
-        <ul class="uk-nav uk-nav-default doc-nav">
-          <li>
-            <a href="doc.html">Translation</a>
-          </li>
-          <li>
-            <a href="doc.html">Customization</a>
-          </li>
-          <li>
-            <a href="doc.html">Development</a>
-          </li>
-          <li>
-            <a href="doc.html">Sources and credits</a>
-          </li>
-        </ul>
-        <h5 class="uk-margin-top">Help</h5>
-        <ul class="uk-nav uk-nav-default doc-nav">
-          <li>
-            <a href="doc.html">Contacting support</a>
-          </li>
-        </ul>
       </div>
     </div>
   </div>
