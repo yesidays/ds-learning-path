3e4cace232dae93267a8550aba2bebb0e0bbe515
Yecely <yecely.diaz@rappi.com>
2020-06-21 19:44:16 -0500

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,11 +47,8 @@ module.exports = {
   modules: [
   ],
   generate: {
-    /*routes: dynamicMarkdownRoutes(),
-    fallback: true, // if you want to use '404.html' instead of the default '200.html'*/
-    routes: dynamicMarkdownRoutes({
-      "/sections": "content/*.md"
-    })
+    routes: dynamicMarkdownRoutes(),
+    fallback: true, // if you want to use '404.html' instead of the default '200.html'
   },
   /*
   ** Build configuration
