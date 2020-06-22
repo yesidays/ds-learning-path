2bc752245d1ab04e7b79fa9ee1479feac00cd12a
Yecely <yecely.diaz@rappi.com>
2020-06-21 19:35:03 -0500

    dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,8 +47,11 @@ module.exports = {
   modules: [
   ],
   generate: {
-    routes: dynamicMarkdownRoutes(),
-    fallback: true, // if you want to use '404.html' instead of the default '200.html'
+    /*routes: dynamicMarkdownRoutes(),
+    fallback: true, // if you want to use '404.html' instead of the default '200.html'*/
+    routes: dynamicMarkdownRoutes({
+      "/sections": "content/*.md"
+    })
   },
   /*
   ** Build configuration
